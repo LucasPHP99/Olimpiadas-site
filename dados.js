b1ff8fb7e442496837150a80897839b2fa136299
LucasPHP99 <124943502+LucasPHP99@users.noreply.github.com>
2024-09-09 21:33:35 +0100

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal",
+        descricao : "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destaca no cenário internacional com sua técnica impecável e determinação. Com apenas poucos anos, ela já conquistou diversas medalhas importantes.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judo judô judoca judoka"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o coração dos brasileiros com sua determinação e talento. Com um histórico de superação impressionante, ela se tornou a primeira ginasta brasileira a conquistar uma medalha de ouro em Jogos Olímpicos.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro"
+    }
+];
+
